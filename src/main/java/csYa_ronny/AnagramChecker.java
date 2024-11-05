f8baafee9d9b03c75ee7b4baf4bd95b23c74a096
tomer <tomerbcool@gmail.com>
2024-11-05 13:13:04 +0200

    finished Q4

--- src/main/java/csYa_ronny/AnagramChecker.java
+++ src/main/java/csYa_ronny/AnagramChecker.java
@@ -2,12 +2,26 @@ package csYa_ronny;
 
 public class AnagramChecker {
     public static boolean areAnagrams(String str1, String str2) {
-        // TODO: Implement the logic to check if str1 and str2 are anagrams
-        return false;  // Return false by default
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+        int[] charCount = new int[256]; // amount of chars in ascii values
+
+        //counting the amount of each char, if an amount goes negative then that is not an engram
+        for (char c : str1.toCharArray()) {
+            charCount[c]++;
+        }
+        for (char c : str2.toCharArray()) {
+            charCount[c]--;
+            if (charCount[c] < 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
-        // דוגמאות לשימוש בשיטה areAnagrams
         String word1 = "listen";
         String word2 = "silent";
         String word3 = "apple";
